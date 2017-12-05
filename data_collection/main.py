21cad6a7e7bc6b076ba85725fe5787a0a4546583
RMCollins175 <rorycollins@me.com>
2017-12-05 16:05:59 +0000

    add monet images from webscraping

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,7 +1,7 @@
 from scripts.artwork_downlaoder import ArtworkDownloader
 
 artwork_downloader = ArtworkDownloader(
-    base_url="https://artuk.org/discover/artworks/view_as/grid/search/makers:laurence-stephen-lowry-18871976/page/9",
-    artist_name="lowry"
+    base_url="https://artuk.org/discover/artworks/view_as/grid/search/makers:claude-monet-18401926-56211/page/3",
+    artist_name="monet"
 )
 artwork_downloader.save_artworks()
