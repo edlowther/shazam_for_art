9f2246034e5c5d798a59340c7b25bf66dc0084d0
RMCollins175 <rorycollins@me.com>
2017-12-05 16:20:36 +0000

    webscrape rembrandt images

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,7 +1,7 @@
 from scripts.artwork_downlaoder import ArtworkDownloader
 
 artwork_downloader = ArtworkDownloader(
-    base_url="https://artuk.org/discover/artworks/view_as/grid/search/makers:claude-monet-18401926-56211/page/3",
-    artist_name="monet"
+    base_url="https://artuk.org/discover/artworks/search/actor:rembrandt-van-rijn-16061669/page/5",
+    artist_name="rembrandt"
 )
 artwork_downloader.save_artworks()
