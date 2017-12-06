b4e517dfef886e927fcf2e258f17df8ab9b56a8e
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-06 12:29:00 +0000

    Consolidate and filter works by all artists in current dataset

--- painting_analysis/main.py
+++ painting_analysis/main.py
@@ -6,9 +6,6 @@ test_image = io.imread('./test_images/lowry_179.jpg')
 clf = joblib.load('model.pkl')
 print(clf.predict([PaintingProcessor(test_image).flatten()]))
 
-
-print(clf.predict([PaintingProcessor(test_image).flatten()]))
-
 # test_image = io.imread('./images/hopper_124.jpg')
 # print(test_image)
 # print(test_image.shape)
