ec5123537c1aac9c92ff21d13d102db346b8a74a
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-07 16:33:25 +0000

    Make DataAssembler class create test data

--- painting_analysis/lib/data_assembler.py
+++ painting_analysis/lib/data_assembler.py
@@ -6,11 +6,15 @@ from random import shuffle
 class DataAssembler:
 
     def load_data(self):
+        artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
         for filename in glob.glob('./images/training_data/*'):
             os.remove(filename)
-        artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
+        for filename in glob.glob('./images/test_data/*'):
+            os.remove(filename)
         for artist in artists:
             filenames = glob.glob('images/all/' + artist + '*')
             shuffle(filenames)
             for filename in filenames[:80]:
                 shutil.copy2(filename, 'images/training_data')
+            for filename in filenames[80:100]:
+                shutil.copy2(filename, 'images/test_data')
