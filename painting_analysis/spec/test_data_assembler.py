042f53452b69ae37c2f07c72e40e99b40fc3c8ee
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-07 21:25:31 +0000

    Refactor DataAssembler tests to rely more on ArtistsLookup class

--- painting_analysis/spec/test_data_assembler.py
+++ painting_analysis/spec/test_data_assembler.py
@@ -17,9 +17,9 @@ def test_load_data_moves_800_paintings_into_the_training_data_folder(data_assemb
     training_data_filenames = os.listdir('./images/training_data')
     assert len(training_data_filenames) == 800
 
-def test_load_data_moves_80_paintings_by_each_artist_into_the_training_data_folder(data_assembler):
+def test_load_data_moves_80_paintings_by_each_artist_into_the_training_data_folder(data_assembler, artist_lookup):
     data_assembler.load_data()
-    artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
+    artists = artist_lookup.keys()
     for artist in artists:
         assert len(glob.glob('images/training_data/' + artist + '*')) == 80
 
@@ -27,9 +27,9 @@ def test_load_data_moves_200_images_into_the_test_data_folder(data_assembler):
     data_assembler.load_data()
     assert len(os.listdir('./images/test_data/')) == 200
 
-def test_load_data_moves_20_paintings_by_each_artist_into_the_test_data_folder(data_assembler):
+def test_load_data_moves_20_paintings_by_each_artist_into_the_test_data_folder(data_assembler, artist_lookup):
     data_assembler.load_data()
-    artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
+    artists = artist_lookup.keys()
     for artist in artists:
         assert len(glob.glob('images/test_data/' + artist + '*')) == 20
 
