e790bbff36f100da702766bd7189605a1cfe39d9
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-07 21:23:47 +0000

    Add implementation of loading targets data

--- painting_analysis/lib/data_assembler.py
+++ painting_analysis/lib/data_assembler.py
@@ -2,16 +2,28 @@ import glob
 import os
 import shutil
 from random import shuffle
+from painting_analysis.lib.artist_lookup import ArtistLookup
 
 class DataAssembler:
+    def __init__(self):
+        self.artist_lookup = ArtistLookup().golden_copy
 
     def load_data(self):
-        artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
+        self._move_files()
+
+    def load_targets(self):
+        targets = []
+        for filename in os.listdir('./images/training_data'):
+            artist_name = filename.split("_")[0]
+            targets.append(self.artist_lookup[artist_name])
+        return targets
+
+    def _move_files(self):
         for filename in glob.glob('./images/training_data/*'):
             os.remove(filename)
         for filename in glob.glob('./images/test_data/*'):
             os.remove(filename)
-        for artist in artists:
+        for artist in self.artist_lookup.keys():
             filenames = glob.glob('images/all/' + artist + '*')
             shuffle(filenames)
             for filename in filenames[:80]:
--- painting_analysis/spec/test_data_assembler.py
+++ painting_analysis/spec/test_data_assembler.py
@@ -2,10 +2,15 @@ import pytest
 import os
 import glob
 from painting_analysis.lib.data_assembler import DataAssembler
+from painting_analysis.lib.artist_lookup import ArtistLookup
 
 @pytest.fixture
 def data_assembler():
     return DataAssembler()
+
+@pytest.fixture
+def artist_lookup():
+    return ArtistLookup().golden_copy
 
 def test_load_data_moves_800_paintings_into_the_training_data_folder(data_assembler):
     data_assembler.load_data()
@@ -27,3 +32,14 @@ def test_load_data_moves_20_paintings_by_each_artist_into_the_test_data_folder(d
     artists = ['constable', 'hopper', 'kandinsky', 'lowry', 'monet', 'rembrandt', 'rothko', 'rubens', 'turner', 'van-gogh']
     for artist in artists:
         assert len(glob.glob('images/test_data/' + artist + '*')) == 20
+
+def test_load_targets_creates_a_list_with_800_elements(data_assembler):
+    targets = data_assembler.load_targets()
+    assert len(targets) == 800
+
+def test_load_targets_assigns_correct_artist_number(data_assembler, artist_lookup):
+    targets = data_assembler.load_targets()
+    training_data_filenames = os.listdir('./images/training_data')
+    for index, filename in enumerate(training_data_filenames):
+        artist_name = filename.split("_")[0]
+        assert targets[index] == artist_lookup[artist_name]
