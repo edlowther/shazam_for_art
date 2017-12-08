9c50c76b3f01d5311a3f0388582d0501c41a0a24
Tim Jones <timjones10@hotmail.com>
2017-12-08 10:51:41 +0000

    Re-setting to build app

--- data_collection/main.py
+++
@@ -1,7 +0,0 @@
-from scripts.artwork_downlaoder import ArtworkDownloader
-
-artwork_downloader = ArtworkDownloader(
-    base_url="https://artuk.org/discover/artworks/search/actor:rembrandt-van-rijn-16061669/page/5",
-    artist_name="rembrandt"
-)
-artwork_downloader.save_artworks()
--- tutorial/main.py
+++
@@ -1,8 +0,0 @@
-from skimage import io
-
-from scripts.painting_processor import PaintingProcessor
-from scripts.model_creator import ModelCreator
-
-clf = ModelCreator().build()
-test_image = io.imread('./test_images/rembrandt_22.jpg')
-print(clf.predict([PaintingProcessor(test_image).flatten()]))
