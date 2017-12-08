476743347bdbe9e20f9a5fe758075ba9aab83674
RMCollins175 <rorycollins@me.com>
2017-12-08 16:40:01 +0000

    update urls

--- image_upload/image_upload/urls.py
+++ image_upload/image_upload/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader import views as uploader_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^upload/$', uploader_views.home, name='imageupload'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
