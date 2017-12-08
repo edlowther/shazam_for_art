1f6e89f9f72c08e2a1d89419daf27cee133c0b41
RMCollins175 <rorycollins@me.com>
2017-12-08 16:41:06 +0000

    update models

--- image_upload/uploader/models.py
+++ image_upload/uploader/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+class Upload(models.Model):
+    pic = models.FileField(upload_to="images/")
+    upload_date=models.DateTimeField(auto_now_add =True)
+
+# FileUpload form class.
+class UploadForm(ModelForm):
+    class Meta:
+        model = Upload
+        fields = ('pic',)
