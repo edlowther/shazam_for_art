404b8ab4cb55b0a58cb7f8e8da1da03eaa83921b
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-10 19:56:37 +0000

    Fix mistake with naming painting_data variable

--- painting_analysis/lib/data_loader.py
+++ painting_analysis/lib/data_loader.py
@@ -20,10 +20,10 @@ class DataLoader:
                 try:
                     filepath = self.directory_path + filename
                     chosen_processing_method = getattr(self.painting_processor_class(filepath), self.method)
-                    data = chosen_processing_method()
+                    painting_data = chosen_processing_method()
                     artist_name = filename.split("_")[0]
                     targets.append(self.artist_lookup[artist_name])
-                    data.append(data)
+                    data.append(painting_data)
                     print(".", end="", flush=True)
                 except WrongColourDepth as e:
                     print ("\nerror processing", filename, "-", type(e))
