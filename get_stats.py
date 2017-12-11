5f34a0bb6e11cb751f12f2a35e6af60477393bf0
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-11 16:29:19 +0000

    Add way to calculate effectiveness of new feature extraction methods

--- get_stats.py
+++ get_stats.py
@@ -2,8 +2,10 @@ from sklearn.externals import joblib
 
 from painting_analysis.lib.stats_generator import StatsGenerator
 
-clf_type = 'neural_network' # available options: svm ; tree ; neural_network
-method = 'grayscale_hog' # available options: flatten_pixels ; grayscale_hog
+# clf_type = 'svm' # available options: svm ; tree ; neural_network
+method = 'texture' # available options: flatten_pixels ; grayscale_hog ; daisy
 
-stats_generator = StatsGenerator(clf_type, method)
-stats_generator.get_accuracy_of_model()
+# for clf_type in ["svm", "tree", "neural_network"]:
+for clf_type in ["svm", "tree"]:
+    stats_generator = StatsGenerator(clf_type, method, "v5")
+    stats_generator.get_accuracy_of_model()
