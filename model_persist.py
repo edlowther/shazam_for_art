30278fb7a4e225098a8acc678c1b25346f63d3ae
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-11 16:29:46 +0000

    Add way to save new feature extraction methods to pkl files

--- model_persist.py
+++ model_persist.py
@@ -7,13 +7,16 @@ from painting_analysis.lib.data_loader import DataLoader
 # method = 'flatten_pixels' # available options: flatten_pixels ; grayscale_hog
 reshuffle_training_data = False
 
-for method in ["flatten_pixels", "grayscale_hog"]:
+# for method in ["flatten_pixels", "grayscale_hog"]:
+for method in ["texture"]:
     # data_loader = DataLoader(method, './images/training_data/')
     # paintings_data = data_loader.load_paintings()
     # targets = data_loader.load_targets()
-    for clf_type in ["svm", "tree", "neural_network"]:
+    for clf_type in ["svm", "tree"]:#, "neural_network"]:
+    # for clf_type in ["tree"]:
         try:
             clf = ModelBuilder(clf_type, method, reshuffle_training_data).build()
-            joblib.dump(clf, './models/{}_{}_v3.pkl'.format(clf_type, method))
-        except:
+            joblib.dump(clf, './models/{}_{}_v5.pkl'.format(clf_type, method))
+        except Exception as err:
             print("failure on", clf_type, method)
+            print(err)
