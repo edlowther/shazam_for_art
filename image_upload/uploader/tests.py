18299d3ff94d597a931d2266d65db24dcf06ae55
RMCollins175 <rorycollins@me.com>
2017-12-11 16:29:27 +0000

    revert back to functional test and scrap unit tests and starting over

--- image_upload/uploader/tests.py
+++ image_upload/uploader/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files import File
+from uploader.models import Upload
 
 # Create your tests here.
