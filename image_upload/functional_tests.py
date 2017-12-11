1624ed243a970ec994d46c30b4f013dc4fb08556
RMCollins175 <rorycollins@me.com>
2017-12-11 14:22:36 +0000

    refactor functional test

--- image_upload/functional_tests.py
+++ image_upload/functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:4567/upload')
 
-assert 'Picture' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:4567/upload')
+
+        self.assertIn('Picture', self.browser.title)
+        # self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+        # [...rest of comments as before]
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:4567/upload')
+#
+# assert 'Picture' in browser.title
