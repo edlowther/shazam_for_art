5cf41627b06d68e0e63f6eaeb2bee707b3a9cb38
Ed Lowther <ed.lowther[at]gmail.com>
2017-12-12 21:40:11 +0000

    Add assignment of painting to new robo-art-historian-generated movements

--- image_uploader/image_uploader/views.py
+++ image_uploader/image_uploader/views.py
@@ -17,7 +17,8 @@ def new(request):
     image_relative_filepath = default_storage.save('tmp/image_to_test.jpg', ContentFile(painting.read()))
     image_absolute_filepath = os.path.join(settings.MEDIA_ROOT, image_relative_filepath)
     predicted_artist_name = make_prediction(image_absolute_filepath)
-    return render(request, 'image_uploader/success.html', {'name': predicted_artist_name})
+    predicted_movement = get_movement(image_absolute_filepath)
+    return render(request, 'image_uploader/success.html', {'name': predicted_artist_name, 'movement': predicted_movement})
 
 
 def make_prediction(image_absolute_filepath):
@@ -32,3 +33,33 @@ def make_prediction(image_absolute_filepath):
                 painting_flattened.append(colour)
     result = clf.predict([painting_flattened])
     return ARTISTS[result[0]]
+
+def get_movement(image_absolute_filepath):
+    MOVEMENTS = ['Expressionistic cubism',
+                 'Romantic colour fields',
+                 'Impressionistic cloisonnism',
+                 'Expressionistic colour fields',
+                 'Cloisonnism-cubism',
+                 'Baroque colour fields',
+                 'Cloisonnistic colour fields',
+                 'Expressionistic-cloisonnistic colour fields',
+                 'Orangey-red colour fields',
+                 'Paler colour fields'
+                 ]
+    GRID_WIDTH = 20
+    painting_data = io.imread(image_absolute_filepath)
+    clf = joblib.load(os.path.join(settings.MEDIA_ROOT, 'unsupervised.pkl'))
+    resized_painting = resize(painting_data, (200, 200))
+    result = []
+    for i in range(10):
+        for j in range(10):
+            grid = resized_painting[
+                i*GRID_WIDTH:i*GRID_WIDTH+GRID_WIDTH,
+                j*GRID_WIDTH:j*GRID_WIDTH+GRID_WIDTH,
+                :
+            ]
+            result.append(grid[:, :, 0].mean())
+            result.append(grid[:, :, 1].mean())
+            result.append(grid[:, :, 2].mean())
+    result = clf.predict([result])
+    return MOVEMENTS[result[0]]
